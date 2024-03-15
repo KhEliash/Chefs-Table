74e334ad513d22baf3ec68a353e6deec9ac69c1f
Kh.Eliash <eliashebrahim@gmail.com>
2024-03-15 23:11:03 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Header = () => {
+    return (
+        <div className="container mx-auto">
+          <h1 className="bg-gray-100">hi</h1>
+        </div>
+    );
+};
+
+export default Header;
